a32f2204922cd75ab6fbc76668bcecef441c9550
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-07-10 23:26:06 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import './index.css';
+
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+  ,
+  document.getElementById('root')
+);
