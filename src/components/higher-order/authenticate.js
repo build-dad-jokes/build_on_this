c4aad845599e7623137640ca66757ced8cf3bd32
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-07-10 23:24:26 -0700

    Create authenticate.js

---
+++ src/components/higher-order/authenticate.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+export default function(ComponentToBeRendered) {
+
+  class Authenticate extends Component {
+    componentWillMount() {
+      if (this.props.loggedIn === false) {
+        this.props.history.push('/login');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (this.props.loggedIn === false) {
+        this.props.history.push('/login');
+      }
+    }
+
+    render() {
+      return (
+        <ComponentToBeRendered {...this.props} />
+      );
+    }
+  }
+
+  function mapStateToProps(state) {
+    return {
+      loggedIn: state.auth.loggedIn,
+      currentUser: state.auth.currentUser
+    };
+  }
+
+  return withRouter(connect(mapStateToProps)(Authenticate))}
