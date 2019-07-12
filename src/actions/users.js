8f953fc021dde7ef010b3b2f1f0b2a4c0b437721
Oliver-Strange <preston.burton@gmail.com>
2019-07-11 20:56:16 -0700

    sets up my heroku url

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,111 +1,109 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://bw-dad-jokes.herokuapp.com';
-
+const BASE_URL = "https://dad-jokes-bw.herokuapp.com";
 
 // READ
 
-export const FETCH_USERS_START = 'FETCH_USERS_START';
-export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-export const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+export const FETCH_USERS_START = "FETCH_USERS_START";
+export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+export const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
 
-
-export const fetchAllUsers = () => (dispatch) => {
-    dispatch({ type: FETCH_USERS_START });
-    const sendToken = {
-      headers: { authorization: localStorage.getItem('jwt') }
-    }
-    axios
-      .get(`${BASE_URL}/api/users`, sendToken)
-      .then(res => {
-        dispatch({
-          type: FETCH_USERS_SUCCESS,
-          payload: res.data.users
-        })
-      })
-      .catch(err => dispatch({
+export const fetchAllUsers = () => dispatch => {
+  dispatch({ type: FETCH_USERS_START });
+  const sendToken = {
+    headers: { authorization: localStorage.getItem("jwt") }
+  };
+  axios
+    .get(`${BASE_URL}/api/users`, sendToken)
+    .then(res => {
+      dispatch({
+        type: FETCH_USERS_SUCCESS,
+        payload: res.data.users
+      });
+    })
+    .catch(err =>
+      dispatch({
         type: FETCH_USERS_FAILURE,
-        payload: err  
-      }))
-  }
+        payload: err
+      })
+    );
+};
 
-  export const FETCH_USER_START = "FETCH_USER_START"
-  export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS"
-  export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE"
+export const FETCH_USER_START = "FETCH_USER_START";
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
 
-
-  export const fetchUserById = (id) => (dispatch) => {
-    dispatch({type: FETCH_USER_START })
-    const sendToken = {
-      headers: { authorization: localStorage.getItem('jwt') }
-  }
-    axios
-        .get(`${BASE_URL}/api/users/${id}`, sendToken)
-        .then(res => {
-            dispatch({
-                type: FETCH_USER_SUCCESS,
-                payload: res
-            })
-        })
-        .catch(err => {
-            dispatch({
-                type: FETCH_USER_FAILURE,
-                payload: err
-            })
-        })
-}
+export const fetchUserById = id => dispatch => {
+  dispatch({ type: FETCH_USER_START });
+  const sendToken = {
+    headers: { authorization: localStorage.getItem("jwt") }
+  };
+  axios
+    .get(`${BASE_URL}/api/users/${id}`, sendToken)
+    .then(res => {
+      dispatch({
+        type: FETCH_USER_SUCCESS,
+        payload: res
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_USER_FAILURE,
+        payload: err
+      });
+    });
+};
 
 // UPDATE
 
-export const UPDATE_USER_START = 'UPDATE_USER_START';
-export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
-export const UPDATE_USER_FAILURE = 'UPDATE_USER_FAILURE';
+export const UPDATE_USER_START = "UPDATE_USER_START";
+export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
+export const UPDATE_USER_FAILURE = "UPDATE_USER_FAILURE";
 
 export const updateUser = (id, changes) => dispatch => {
-  dispatch({ type: UPDATE_USER_START});
+  dispatch({ type: UPDATE_USER_START });
   const sendToken = {
-      headers: { authorization: localStorage.getItem('jwt') }
-  }
+    headers: { authorization: localStorage.getItem("jwt") }
+  };
   axios
     .put(`${BASE_URL}/api/users/${id}`, changes, sendToken)
-    .then(res =>{
+    .then(res => {
       dispatch({
         type: UPDATE_USER_SUCCESS,
         payload: res.data
-      })
+      });
     })
-    .catch(err => dispatch({
-      type: UPDATE_USER_FAILURE,
-      payload: err
-    }))
-}
+    .catch(err =>
+      dispatch({
+        type: UPDATE_USER_FAILURE,
+        payload: err
+      })
+    );
+};
 
 // DELETE
 
-export const DELETE_USER_FAILURE = 'DELETE_USER_FAILURE';
-export const DELETE_USER_START = 'DELETE_USER_START';
-export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+export const DELETE_USER_FAILURE = "DELETE_USER_FAILURE";
+export const DELETE_USER_START = "DELETE_USER_START";
+export const DELETE_USER_SUCCESS = "DELETE_USER_SUCCESS";
 
-
-export const deleteUser = (id) => dispatch => {
-  dispatch({ type: DELETE_USER_START});
+export const deleteUser = id => dispatch => {
+  dispatch({ type: DELETE_USER_START });
   const sendToken = {
-      headers: { authorization: localStorage.getItem('jwt') }
-  }
+    headers: { authorization: localStorage.getItem("jwt") }
+  };
   axios
     .delete(`${BASE_URL}/api/users/${id}`, sendToken)
-    .then(res =>{
+    .then(res => {
       dispatch({
         type: DELETE_USER_SUCCESS,
         payload: id
-      })
+      });
     })
-    .catch(err => dispatch({
-      type: DELETE_USER_FAILURE,
-      payload: err
-    }))
-}
-
-
-
-
+    .catch(err =>
+      dispatch({
+        type: DELETE_USER_FAILURE,
+        payload: err
+      })
+    );
+};
--- src/components/presentational/Jokes.js
+++ src/components/presentational/Jokes.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { deleteJoke } from '../../actions';
-import Joke from './Joke';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { deleteJoke } from "../../actions";
+import Joke from "./Joke";
 
 export class Jokes extends Component {
-    render() {
-      return (
-        <div className="jokes-list">
-          <h1>JOKES FROM YA DADDY</h1>
-          <ul>
-            {this.props.jokes.map(joke => {
-              return (
-                <Joke
-                  question={joke.question}
-                  id={joke.id}
-                  answer={joke.answer}
-                  key={joke.id}
-                  created_by={joke.created_by}
-                />
-              );
-            })}
-          </ul>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="jokes-list">
+        <h1>JOKES FROM YA DADDY</h1>
+        <ul>
+          {this.props.jokes.map(joke => {
+            return (
+              <Joke
+                question={joke.joke}
+                id={joke.id}
+                answer={joke.punchline}
+                key={joke.id}
+                created_by={joke.created_by}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
-
-const mapStateToProps = state => {
-    return state.auth
 }
 
+const mapStateToProps = state => {
+  return state.auth;
+};
 
-export default connect(mapStateToProps, { deleteJoke })(Jokes);
+export default connect(
+  mapStateToProps,
+  { deleteJoke }
+)(Jokes);
